d3d03ceaf0c9d94ed43adfc24bde187a41291ab6
JenAdamenko <adamiea8774@gmail.com>
2022-09-19 19:56:15 +0300

    вносит измнения по критериям

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,9 @@ const html = () => {
 
 // Scripts
 const scripts = () => {
-  return gulp.src("source/js/*.js").pipe(terser()).pipe(gulp.dest("build/js"));
+  return gulp.src("source/js/*.js")
+  .pipe(terser())
+  .pipe(gulp.dest("build/js"));
   // .pipe(browser.stream());
 };
 
